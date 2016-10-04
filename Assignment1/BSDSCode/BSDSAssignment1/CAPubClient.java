2bb62e7ad819a04e8f79901176724857f16ef757
Soumya Ramesh <soumya@ccs.neu.edu>
2016-10-04 00:37:58 -0400

    registered new publishers and published 10k messages

--- Assignment1/BSDSCode/BSDSAssignment1/CAPubClient.java
+++ Assignment1/BSDSCode/BSDSAssignment1/CAPubClient.java
@@ -22,15 +22,26 @@ public class CAPubClient {
     public static void main(String[] args) {
 
         String host = (args.length < 1) ? null : args[0];
+        int numberOfMessages = Integer.parseInt(args[1]);
         try {
             System.out.println ("Publisher Client Starter");
             Registry registry = LocateRegistry.getRegistry(host, 1099);
              System.out.println ("Connected to registry");
             BSDSPublishInterface CAServerStub = (BSDSPublishInterface) registry.lookup("CAServerPublisher");
             System.out.println ("Stub initialized");
-            int id = CAServerStub.registerPublisher("Sporty Stuff", "Soccer");
-            System.out.println("Pub id = " + Integer.toString(id));
-            CAServerStub.publishContent(id, "Title", "Message", 30);
+            int id1 = CAServerStub.registerPublisher("Publisher1", "Soccer");
+            System.out.println("Registered Pub id = " + Integer.toString(id1));
+            int id2 = CAServerStub.registerPublisher("Publisher2", "Soccer");
+            System.out.println("Registered Pub id = " + Integer.toString(id2));
+            int id3 = CAServerStub.registerPublisher("Publisher3", "News");
+            System.out.println("Registered Pub id = " + Integer.toString(id3));
+
+            for (int i = 0; i < numberOfMessages; i++) {
+                CAServerStub.publishContent(id1, "Title", "Message", 30);
+                CAServerStub.publishContent(id2, "Title", "Message", 30);
+                CAServerStub.publishContent(id3, "Title", "Message", 30);
+            }
+
             System.out.println("Publish Client is working");
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
