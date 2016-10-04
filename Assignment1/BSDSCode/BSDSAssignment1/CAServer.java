32d6594fc6ca41ec64adce586ed6e4c5f8cdcea9
Soumya Ramesh <soumya@ccs.neu.edu>
2016-10-04 00:38:24 -0400

    implemented registerPublisher, subscriber and publishContent

--- Assignment1/BSDSCode/BSDSAssignment1/CAServer.java
+++ Assignment1/BSDSCode/BSDSAssignment1/CAServer.java
@@ -7,46 +7,61 @@ import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.Queue;
+import java.util.Set;
+import java.util.UUID;
 
 //Skeleton of CAServer supporting both BSDS interfaces
 
-public class CAServer implements BSDSPublishInterface, BSDSSubscribeInterface{
+public class CAServer implements BSDSPublishInterface, BSDSSubscribeInterface {
 
-    public int registerPublisher(String name, String topic) 
-            throws RemoteException {
+    HashMap<Integer,String> publisherToTopic = new HashMap<>();
+    HashMap<Integer,String> subscriberToTopic = new HashMap<>();
+    HashMap<String,Queue<BSDSContent>> topicQueues = new HashMap<>();
 
-        System.out.println(
-                        "Publisher: " +
-                        name +
-                        topic
-        );
-        return 11;
+
+    // registers a publisher and returns the id
+    public int registerPublisher(String name, String topic) throws RemoteException {
+        System.out.println("Publisher: " + name + topic);
+        int pubId = Integer.parseInt(UUID.randomUUID().toString());
+        publisherToTopic.put(pubId,topic);
+        return pubId;
     }
+
 
     // publishes a message to the server
-     public void  publishContent (int publisherID, String title,String message, int TimeToLive) 
-             throws RemoteException {
-
-        System.out.println ("Message:" +
-                title  +
-                message
-        );
+    public void publishContent(int publisherID, String title, String message, int TimeToLive)
+            throws RemoteException {
+        String topic = publisherToTopic.get(publisherID);
+        if (topicQueues.get(topic).isEmpty()) {
+            throw new NullPointerException("Could not find publisher for this topic");
+        }
+        else {
+            topicQueues.get(topic).add(new BSDSContent(title, message, TimeToLive));
+            System.out.println("Message:" +
+                    title +
+                    message
+            );
+        }
     }
 
-     public int registerSubscriber (String topic) throws RemoteException {
-         System.out.println("Topic is  " + topic);
-         return 1;
-     }
+    public int registerSubscriber(String topic) throws RemoteException {
+        System.out.println("Topic is  " + topic);
+        Integer subId = Integer.parseInt(UUID.randomUUID().toString());
+        subscriberToTopic.put(subId,topic);
+        return subId;
+    }
+
 
     // gets next outstanding message for a subscription
     public String getLatestContent(int subscriberID) throws RemoteException {
-        String message = "Title: Soccer scores, Message: City win 8 straight games"; 
+        String message = "Title: Soccer scores, Message: City win 8 straight games";
         System.out.println("returning latest content");
         return message;
     }
 
     public static void main(String args[]) {
-
         try {
             CAServer objPub = new CAServer();
             CAServer objSub = new CAServer();
@@ -55,14 +70,14 @@ public class CAServer implements BSDSPublishInterface, BSDSSubscribeInterface{
             BSDSSubscribeInterface sStub = (BSDSSubscribeInterface) UnicastRemoteObject.exportObject(objSub, 0);
             System.out.println("stubs created ....");
             // Bind the remote object's stub in the local host registry
-            LocateRegistry.createRegistry(1099);  
-                
-            Registry registry = LocateRegistry.getRegistry();  
-            System.out.println("Ref to Registry ok");  
+            LocateRegistry.createRegistry(1099);
+
+            Registry registry = LocateRegistry.getRegistry();
+            System.out.println("Ref to Registry ok");
             try {
                 registry.bind("CAServerPublisher", pStub);
                 registry.bind("CAServerSubscriber", sStub);
-            } catch(Exception e) {
+            } catch (Exception e) {
                 System.out.println("Caught already bound exception, probably safe to continue in dev mode" + e.toString());
             }
             System.err.println("CAServer ready");
